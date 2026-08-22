a6f4372205e3f4899a08523585673e80df4cc377
laavanjan <130158996+laavanjan@users.noreply.github.com>
2026-08-23 00:49:32 +0530

    header updated

--- src/Pages/Books.js
+++ src/Pages/Books.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 
 import BooksContainer from "../Components/book/books-container/BooksContainer";
 import Intro from "../Components/book/intro/Intro";
@@ -7,6 +8,17 @@ import SubmitArticleBanner from "../Components/book/submit-banner/SubmitArticleB
 const Books = () => {
     return (
         <div>
+        <Helmet>
+            <title>நூல்கள் | தமிழ் இலக்கிய மன்றம்</title>
+            <meta
+                name="description"
+                content="தமிழ் இலக்கிய மன்றத்தின் நூல்கள் மற்றும் படைப்பு சமர்ப்பிப்பு"
+            />
+            <meta
+                name="keywords"
+                content="TLA, Tamil Literary Association, Books, தமிழருவி"
+            />
+        </Helmet>
         <Intro />
         <BooksContainer />
         <SubmitArticleBanner />
